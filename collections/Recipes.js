82341564f8a7f4d029f1b85cf31ad5174f1ef3bb
cortesa <cortesa@gmail.com>
2016-03-20 23:38:41 +0100

    Initial Commit

---
+++ collections/Recipes.js
@@ -0,0 +1,61 @@
+Recipes = new Mongo.Collection('recipes');
+
+Recipes.allow({
+	insert: function(userId, doc){
+		return !!userId;
+	}
+});
+
+Ingridient = new SimpleSchema({
+	name:{
+		type:String
+	},
+	amount:{
+		type: String
+	}
+});
+
+
+RecipesSchema = new SimpleSchema({
+	name: {
+		type: String,
+		label: "Name"
+	},
+	desc:{
+		type: String,
+		label:"Description"
+	},
+	ingridient:{
+		type:[Ingridient]
+	},
+	inMenu:{
+		type: Boolean,
+		defaultValue: false,
+		optional:true,
+		autoform: {
+			type: "hidden"
+		}
+	},
+	author: {
+		type:  String,
+		label: "Author",
+		autoValue: function(){
+			return this.userId
+		},
+		autoform: {
+			type:"hidden"
+		}
+	},
+	createdAt:{
+		type: Date,
+		label:"created At",
+		autoValue: function(){
+			return new Date()
+		},
+		autoform: {
+			type:"hidden"
+		}
+	}
+});
+
+Recipes.attachSchema( RecipesSchema );
